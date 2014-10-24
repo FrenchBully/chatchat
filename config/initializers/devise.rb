1f513103d199d68e9f59aaef4786db0613b41cde
andycanderson <chen.a.andy@gmail.com>
2014-10-23 23:00:26 -0700

    changing keys back to new meetup keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,9 +234,9 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  config.omniauth :meetup, 'n5qbl8a65gcmjlp8v779sm66o4', 'enqesvtmg4d9h7kojk91bn0cb4'
+  # config.omniauth :meetup, 'n5qbl8a65gcmjlp8v779sm66o4', 'enqesvtmg4d9h7kojk91bn0cb4'
 
-  # config.omniauth :meetup, 'eq2t7ckvp7ojknr5bs8i0g09bm', 'e2vbregc388e125monnnl1jbsl'
+  config.omniauth :meetup, 'eq2t7ckvp7ojknr5bs8i0g09bm', 'e2vbregc388e125monnnl1jbsl'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
