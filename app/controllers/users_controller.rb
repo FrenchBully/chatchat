91eb7c4d2f490d248427ab2e5443a07e71761b3a
craynkshaft <craynkshaft@gmail.com>
2014-10-24 15:10:14 -0700

    intiializer into the controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,8 @@ class UsersController < ApplicationController
 
   def edit
     MeetupClient.configure do |config|
-  # Ran's => config.api_key = 'b7d767765272d2b2b13c25e744e13'
-  config.api_key = 'b7d767765272d2b2b13c25e744e13'
+    # Ran's => config.api_key = 'b7d767765272d2b2b13c25e744e13'
+    config.api_key = 'b7d767765272d2b2b13c25e744e13'
 end
   	@user = User.find(params[:id])
     @interests = Interest.all
