3386e5ab82e1b6573c9a21787354a1f417c39348
FrenchBully <kvng88@gmail.com>
2014-10-25 12:26:34 -0700

    added event_name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024004240) do
+ActiveRecord::Schema.define(version: 20141025192319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20141024004240) do
     t.string   "location"
     t.integer  "expires_at"
     t.string   "event_id"
+    t.string   "event_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
