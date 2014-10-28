24e6ad10f2aa4c3b5853221e6433b3567052eab9
FrenchBully <kvng88@gmail.com>
2014-10-27 17:53:07 -0700

    setup of linkedin oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :omniauthable,
+  devise :database_authenticatable, :registerable, :omniauthable, 
          :recoverable, :rememberable, :trackable, :validatable
 
   has_and_belongs_to_many :interests, through: :interests_users
