6710b69932b25464ff9cf1e56aab32bd2428aa3e
craynkshaft <craynkshaft@gmail.com>
2014-10-29 21:41:49 -0700

    removed bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,7 +85,7 @@ class User < ActiveRecord::Base
     options = { 
         member_id: user.uid,
 
-        time: "0d,10d"
+        time: "0d,20d"
     }
     meetup_api = MeetupApi.new
     return meetup_api.events(options)
